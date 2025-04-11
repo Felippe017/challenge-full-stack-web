06428bbd5dd443e2f440e6ae900fd09e54a7c687
Felippe <felippe.correa@virotrip.com.br>
2025-04-10 21:22:11 -0300

    feat(createStudents): refactor index.ts and add healthcheck router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from 'express';
-import { prisma } from './lib/prisma';
+import express from 'express';
+import studentsRouter from './routes/students'
 
 const app = express()
 const port = 3000
@@ -7,11 +7,12 @@ const port = 3000
 
 app.use(express.json())
 
-app.get('/students', async (req: Request, res: Response) => {
-    const users = await prisma.students.findMany()
-    res.json(users)
-})
+app.get("/healthcheck", (_req, res) => {
+  res.send("API is up and running!");
+});
+
+app.use("/students", studentsRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App is running on port ${port}`)
 })
