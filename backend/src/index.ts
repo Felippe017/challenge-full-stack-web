59bd6e18fa69bfac727f7dc12ca295f8621b5e3b
Felippe <felippe.correa@virotrip.com.br>
2025-04-12 19:53:45 -0300

    feat(studentsListScreen): install cors and change port of localhost

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import cors from 'cors'
 import studentsRouter from './routes/students'
 
 const app = express()
-const port = 3000
+const port = 8000
 
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/healthcheck", (_req, res) => {
   res.send("API is up and running!");
