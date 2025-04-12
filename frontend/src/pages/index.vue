aefb05b9baad82ec87854f5bcf0c752fe68fd412
Felippe <felippe.correa@virotrip.com.br>
2025-04-12 15:28:03 -0300

    feat(sideBarMenu): adjust config global of app

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,7 +1,29 @@
 <template>
-  <NavigatorDrawer />
+  <v-main class="main-content">
+    <v-img
+      alt="Logo Mais A Educação"
+      class="image-education"
+      src="https://maisaedu.com.br/hubfs/site-grupo-a/logo-mais-a-educacao.svg"
+    />
+    <NavigatorDrawer />
+  </v-main>
 </template>
 
 <script lang="ts" setup>
   //
 </script>
+
+<style lang="sass">
+.main-content
+  margin-top: 40px
+  display: flex
+  flex-direction: column
+  align-items: flex-start
+  padding: 50px
+
+.image-education
+  height: 95px
+  width: 95px
+  margin-left: 75px
+  margin-bottom: 20px
+</style>
