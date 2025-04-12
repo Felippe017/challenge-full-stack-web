da2de89bbb88b0d487e7bfe90480496fa477b31b
Felippe <felippe.correa@virotrip.com.br>
2025-04-12 16:43:44 -0300

    feat(studentsListScreen): adjust navigation drawer component with global app and add logic to add component screen list on side

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,16 +1,23 @@
 <template>
   <v-main class="main-content">
     <v-img
-      alt="Logo Mais A Educação"
+      alt="Logo A+ Educação"
       class="image-education"
       src="https://maisaedu.com.br/hubfs/site-grupo-a/logo-mais-a-educacao.svg"
     />
-    <NavigatorDrawer />
+    <NavigatorDrawer @change-view="currentView = $event">
+      <StudentsList
+        v-if="currentView === 'list'"
+        class="view-container"
+      />
+    </NavigatorDrawer>
+
   </v-main>
 </template>
 
 <script lang="ts" setup>
-  //
+  import { ref } from 'vue'
+  const currentView = ref('list')
 </script>
 
 <style lang="sass">
@@ -19,11 +26,16 @@
   display: flex
   flex-direction: column
   align-items: flex-start
-  padding: 50px
+  padding: 0 100px
+
 
 .image-education
   height: 95px
   width: 95px
   margin-left: 75px
   margin-bottom: 20px
+
+.view-container
+  margin-left: 300px
+  width: 100%
 </style>
